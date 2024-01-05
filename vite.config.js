c31722d47ad05d50a25f47f1376892ab48e4b902
Bayastan123456 <127396034+Bayastan123456@users.noreply.github.com>
2024-01-05 16:45:26 +0300

    fefgg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
-  base: "/my-app/",
-};
+export default defineConfig({
+  plugins: [react()],
+});
